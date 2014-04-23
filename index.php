46e8a486f2dc7b9fb35dd19bf17acbfffd037386
emrysblack <emrysblack@gmail.com>
2014-04-23 15:45:11 -0600

    fixed index

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <!doctype html>
-<?xml version = "1.0" encoding = "utf-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
-          "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html lang="en">
 <!--
    Hello World Test Page
   -->
-<html xmlns = "http://www.w3.org/1999/xhtml">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>My OpenShift Home</title>
   <head>
     <title>Hello World</title>
   </head>
