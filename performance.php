f129a4752deb1ceecd5b4dc232efa1fa69fbb3d1
emrysblack <emrysblack@gmail.com>
2014-05-31 12:06:59 -0600

    pushing test html for access

---
+++ performance.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+<!--
+   My home page
+  -->
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <script type = "text/javascript" src = "iJs.js"></script>
+  <link href = "iStyle.css" type = "text/css" rel = "stylesheet">
+  <title>Voting</title>
+  </head>
+  <body>
+    <h1>CS 313</h1>
+    <div id="links">
+    <div>
+      <a href = "index.php">Home</a>
+      </div>
+      <div>
+      <a href = "assignments.html">Assignments</a>
+      </div>
+    </div>
+    <hr/>
+    <div>
+    <p class="p1">
+    Search Performances
+    <br/>
+    <div style="margin:auto; border:2px solid #a1a1a1; 
+                padding:10px 40px; background:#dddddd; 
+                width:300px; border-radius:25px">
+    <form action="" method="GET">
+       <input type="text" name="name" placeholder="name"/><br/><br/>
+       <input type="submit" value="Submit"/>
+    </form>
+    </div>
+    </p>
+    </div>
+    <?php
+      try
+      {
+         $username = "adminHjLJ4vT";
+         $password = "45sj_EbggidU";
+         $conn = new PDO('mysql:host=localhost;dbname=php', $username, $password);
+         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      }
+      catch(PDOException $e)
+      {
+         echo 'ERROR: ' . $e->getMessage();
+      }
+    ?>
+  </body>
+</html>
