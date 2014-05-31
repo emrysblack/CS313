6d5d924899d00c5278d0c6cb652df4aecb2e59b3
emrysblack <emrysblack@gmail.com>
2014-05-31 12:57:18 -0600

    updating page

--- performance.php
+++ performance.php
@@ -52,13 +52,13 @@
             die("Could not connect to database");
          }
          $pdo = $db->prepare("SELECT * FROM Student WHERE name=:name");
-         $pdo->bindValue(':name', $_POST["name"], PDO::PARAM_VARCHAR(100));
+         $pdo->bindValue(':name', $_POST["name"]);
          $pdo->execute();
          $rows = $pdo->fetchAll(PDO::FETCH_ASSOC);
-//         foreach($rows as $row)
-//         {
-//            print $row . <br/>
-//         }
+        foreach($rows as $row)
+         {
+            print $row['name'] . <br/>
+         }
       }
     ?>
   </body>
